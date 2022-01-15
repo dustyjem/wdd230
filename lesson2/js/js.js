eca0f299e77c6a1d2cfe7554ebe3a8c0ec78366a
dustyjem <97073289+dustyjem@users.noreply.github.com>
2022-01-15 15:36:44 -0700

    Lesson 2 Assignment

---
+++ lesson2/js/js.js
@@ -0,0 +1,5 @@
+let string = document.lastModified;
+document.querySelector("#updates").innerHTML = (document.lastModified);
+
+let thisyear = new Date().getFullYear();
+document.querySelector("#thisyear").innerHTML = (thisyear);
