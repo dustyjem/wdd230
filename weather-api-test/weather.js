5e625159b38176feb499d0ba81b56d74c4081a6f
dustyjem <97073289+dustyjem@users.noreply.github.com>
2022-03-11 10:32:08 -0700

    weather Api applied

--- weather-api-test/weather.js
+++ weather-api-test/weather.js
@@ -7,11 +7,22 @@ fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
-        document.querySelector('#current-temp').textContent = ((jsObject.main.temp - 273.15) * 1.8 + 32).toFixed(1);
+        const t = jsObject.main.temp.toFixed(1);
+        document.querySelector('#temp').textContent = t;
+
         const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
         const desc = jsObject.weather[0].description;
-        document.querySelector('#icon-src').textContent = iconsrc;
+        const windsp = jsObject.wind.speed;
         document.querySelector('#weathericon').setAttribute('src', iconsrc);
         document.querySelector('#weathericon').setAttribute('alt', desc);
-        document.querySelector('figcaption').textContent = desc;
+        document.querySelector('#desc').textContent = desc;
+        document.querySelector('#speed').textContent = windsp;
+
+
+        if (t <= 50 && windsp > 3) {
+            const windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(windsp, 0.16) + 0.4275 * t * Math.pow(windsp, 0.16)
+            document.querySelector("#windchill").innerHTML = `${Math.round(windchill)}&#176;`;
+        } else {
+            document.querySelector("#windchill").innerHTML = "N/A"
+        }
     });
